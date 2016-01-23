4f71c1fe61fde320e3d3934c78dafd381d3ac8a4
Michelle Schuknecht <mschuknecht@kavi.com>
2016-01-22 16:47:03 -0800

    added number check

--- calculator.rb
+++ calculator.rb
@@ -1,48 +1,60 @@
 # create a calculator
 
 def prompt(msg)
-  Kernel.puts("=> #{msg}")
+  puts("=> #{msg}")
 end
 
 def calculate(num1, num2, action)
-  if is_num?(num1) && is_num?(num2)
-    case action
-      when '+'
-        prompt(num1.to_i + num2.to_i)
-      when '-'
-        prompt(num1.to_i - num2.to_i)
-      when  '*'
-        prompt(num1.to_i * num2.to_i)
-      when '/'
-      prompt(num1.to_f / num2.to_f)
-      else
-      prompt("I do not understand the action you would like to perform.  Please try again.")
-    end
-  else
-    prompt("You did not enter a number.  Please try again.")
+  case action
+    when '+'
+      prompt(num1.to_f + num2.to_f)
+    when '-'
+      prompt(num1.to_f - num2.to_f)
+    when  '*'
+      prompt(num1.to_f * num2.to_f)
+    when '/'
+    prompt(num1.to_f / num2.to_f)
+    else
+    prompt("I do not understand the action you would like to perform.  Please try again.")
   end
 end
 
-def is_num?(str)
-  /\d$/ === str
+def integer?(str)
+  /^\d+$/.match(str)
+end
+
+def float?(str)
+  /\d/.match(str) && /^\d*\.?\d*$/.match(str)
+end
+
+def number?(str)
+  integer?(str) || float?(str)
 end
 
 continue = 'y'
 
 until continue.downcase == 'n'
-  prompt("Please enter a number")
-  num1 = Kernel.gets().chomp.()
 
-  prompt("Please enter another number")
-  num2 = Kernel.gets().chomp()
+  num1 = ''
+  until number?(num1)
+    prompt("Please enter a number")
+    num1 = gets.chomp
+  end
 
-  prompt("Please enter the action you would like to perform")
-  action = Kernel.gets().chomp()
+  num2 = ''
+  until number?(num2)
+    prompt("Please enter another number")
+    num2 = gets.chomp
+  end
+
+
+  prompt("Please enter the action you would like to perform: '+', '-', '*', '/' ")
+  action = gets.chomp
 
   calculate(num1, num2, action)
 
   prompt("Would you like to continue? y for yes, n to exit ")
-  continue = Kernel.gets().chomp()
+  continue = gets.chomp
 end
 
 
